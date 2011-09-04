c8fefb5cd75e5621392b1e22309b51459948d20a
Christoph Rosse <christoph@rosse.at>
2011-09-04 17:15:36 +0200

    fixed bracket indentation

--- config/config.php
+++ config/config.php
@@ -9,9 +9,12 @@ $this->dispatcher->connect('context.load_factories', function(sfEvent $event)
 	set_error_handler(array($errorHandler, 'handleError'), E_ALL | E_STRICT);
 	register_shutdown_function(array($errorHandler, 'handleShutdown'));
 	
-	if (sfConfig::get('global_error_handler_force_exceptions', false)) {
+	if (sfConfig::get('global_error_handler_force_exceptions', false)) 
+	{
 		$dispatcher->connect('error_handler.handle_error', array($errorHandler, 'handleAsException'));
-	} else {
+	} 
+	else 
+	{
 		$dispatcher->connect('config.global_configuration_loaded', function(sfEvent $event) use ($dispatcher, $errorHandler)
 		{
 			if (sfConfig::get('global_error_handler_force_exceptions', false))
